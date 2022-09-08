bfad2b8e0e142a392fd08a742fc79148bd3c3e70
phuoc` Minh <duckstuckinbucket@gmail.com>
2022-09-08 01:57:46 +0000

    Update 2 files

---
+++ src/components/slide.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+ const Slide:React.FC = () => {
+    
+    
+    return <>
+    
+    </>
+} 
+
+export default Slide;
--- src/store/useReducer.tsx
+++ src/store/useReducer.tsx
@@ -1,4 +1,6 @@
 import {useReducer} from 'react';
 
 
-interface 
+interface contextType {
+    
+}
